79ce15be4e5f68b8838fdb8a3e5cf8274c118483
Ayush <ayushjn20.uec2016@iitr.ac.in>
2017-07-01 09:01:20 +0530

    improved, switched to wsgi

--- newsapp/wsgi.py
+++ newsapp/wsgi.py
@@ -1,14 +1,9 @@
-"""
-WSGI config for newsapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsapp.settings")
 
-from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
