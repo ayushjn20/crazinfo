77afeeb026ad7c5b0e16a2385fb01558af368ea4
Ayush <ayushjn20.uec2016@iitr.ac.in>
2017-07-01 07:32:23 +0530

    improved requirements.txt -4

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -135,10 +135,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-	os.path.join(BASE_DIR,"static"),
-)
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
